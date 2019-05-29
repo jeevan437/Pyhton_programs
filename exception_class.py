29e01b2fec94d44f2edce20a93b5659dbb0442fb
jeevan437 <jeevanpjr@gmail.com>
2019-05-29 06:49:32 +0530

    Exception_base class

---
+++ exception_class.py
@@ -0,0 +1,16 @@
+a=int(input("enter a value:"))
+b=int(input("enter b value::"))
+try:
+    c=a/b
+    print(c)
+# except Exception as e:
+except ZeroDivisionError:
+    print("Denominator must be greater than 0")
+
+    # print(type(e).__name__)
+
+else:
+    print("no exception")
+
+finally:
+    print("closing all the connectios")
