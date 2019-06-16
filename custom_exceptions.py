22be285a8c55ff40f100a2f0405ecfa51f01b1e8
jeevan437 <jeevanpjr@gmail.com>
2019-06-16 08:03:19 +0530

    Custom exceptions

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -10,22 +10,22 @@ class ValueToolargeError(Error):
     #Raised when the input value is too large
     pass
 
+class CustomException:
+    number =10
+    n = int(input("enter n value::"))
+    try:
+        if n < number :
+            raise ValueTooSmallError
+        elif n > number:
+            raise ValueToolargeError
+        else :
+            print("the given input matches the number")
 
-number =10
-n = int(input("enter n value::"))
-try:
-    if n < number :
-        raise ValueTooSmallError
-    elif n > number:
-        raise ValueToolargeError
-    else :
-        print("the given input matches the number")
 
+    except ValueTooSmallError:
+        print("This value is too small, try again")
 
-except ValueTooSmallError:
-    print("This value is too small, try again")
+    except ValueToolargeError:
+        print("This value is too large, Try again")
 
-except ValueToolargeError:
-    print("This value is too large, Try again")
-
-print("congrtes you guessed it correctly..")
+    print("congrtes you guessed it correctly..")
