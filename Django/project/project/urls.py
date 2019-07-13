26144582e067038f42578cc751784e99e233bf1c
jeevan437 <jeevanpjr@gmail.com>
2019-07-13 14:49:23 +0530

    added templates for login and logout

--- Django/project/project/urls.py
+++ Django/project/project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import url,include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('accounts/', include('django.contrib.auth.urls')),
+    url('', TemplateView.as_view(template_name='home.html'), name='home'),
+
 ]
